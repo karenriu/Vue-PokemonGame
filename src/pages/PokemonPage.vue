480793519d5db3f207f2ad2baa91a0914266042d
karen Rios <krios@secundus.com.mx>
2023-08-02 12:13:03 -0600

    structure and obtaining images of the pokemons

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>¿Quién es este Pokemon?</h1>
+
+  <PokemonPicture :pokemonId="251" :showPokemon="true" />
+
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonPicture from "@/components/PokemonPicture";
+import PokemonOptions from "@/components/PokemonOptions";
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+console.log(getPokemonOptions());
+
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+};
+</script>
