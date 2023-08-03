7617b315fd04c1c7435664b6eb5e40142a05dc66
karen Rios <krios@secundus.com.mx>
2023-08-03 11:28:22 -0600

    unit test

---
+++ tests/unit/api/components/PokemonPicture.spec.js
@@ -0,0 +1,19 @@
+import { shallowMount } from "@vue/test-utils";
+import PokemonPicture from "@/components/PokemonPicture";
+
+describe("PokemonPicture component", () => {
+  test("debe de hacer match con el snapshot", () => {
+    const wrapper = shallowMount(PokemonPicture, {
+      props: {
+        pokemonId: 1,
+        showPokemon: false,
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test("debe de mostrar la imagen oculta y el pokemon 100", () => {});
+
+  test("debe de mostrar el pokemon si el ShowPokemon: TRUE", () => {});
+});
